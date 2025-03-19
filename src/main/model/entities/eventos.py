2ba43f66eca4e8e89fe92832ee6569240b548a3b
denisoliveiraa <denis_o@hotmail.com.br>
2025-03-18 23:43:17 -0300

    feature: unit test route post subscription

--- src/main/model/entities/eventos.py
+++
@@ -1,7 +0,0 @@
-from src.model.configs.base import base
-from sqlalchemy import Column, String, Integer
-class Eventos(Base):
-  __tablename__ = "Eventos"
-
-  id =  Column(Integer, primary_key=True, autoincrement=True)
-  nome = Column(String, nullable=False)
--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify
-
+ 
 event_route_bp = Blueprint("event_route", __name__)
-
+ 
+ 
 @event_route_bp.route("/event", methods=["POST"])
 def create_new_event():
-    return jsonify({"esotu": "aqui"}), 201
-
+     return jsonify({ "estou": "aqui" }), 201 # noqa
