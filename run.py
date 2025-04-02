8dbfbbecae624aa66b3fb66fd974c6cd40d04435
denisoliveiraa <denis_o@hotmail.com.br>
2025-04-01 22:34:16 -0300

    feat: validator to event

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+import sys
+
+print("Rodando run.py correto!", file=sys.stderr)
 from src.main.server.server import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=int(3000), debug=True, use_reloader=False)
